f29179f4354adaee5086e9ffb843aac6e70d8509
DfrtJulien <de.freitas.julien78@gmail.com>
2025-01-10 20:59:46 +0100

    page qui sommes nous terminé

---
+++ src/Views/annexes/quiSommesNous.view.php
@@ -0,0 +1,35 @@
+<?php
+require_once(__DIR__ . "/../partials/head.php");
+
+?>
+
+<section class="header">
+  <img src="/public/img/header-QSM.png" alt="deux bagues" class="header-img">
+</section>
+<section class="qsm">
+  <h1>Qui sommes-nous ?</h1>
+  <p class="first-p">Découvrez notre famille portugaise, passionnée par la tradition et l'artisanat. Depuis des générations, nous créons des bijoux traditionnels, alliant savoir-faire ancestral et amour du détail. Chaque pièce raconte une histoire unique, reflétant la richesse de notre culture. Nous partageons avec fierté l'héritage portugais à travers nos créations authentiques et intemporelles.</p>
+  <div class="qsm-flex">
+    <div class="qsm-img">
+      <img src="/public/img/sqm-portugal.png" alt="image d'une rue du Portugal">
+    </div>
+    <p>Le Portugal, c'est bien plus qu'un pays, c'est une terre de traditions, de fado et de paysages à couper le souffle. Chaque coin respire l'histoire et la chaleur de son peuple. En tant que Portugais, je suis fier de notre culture riche, de nos plages dorées et de l'âme accueillante de notre nation.</p>
+  </div>
+  <div class="qsm-flex">
+    <p>L'amour des bijoux traditionnels est un lien profond avec notre héritage. Chaque pièce est une œuvre d'art, façonnée avec soin, portant l'empreinte de générations passées. Porter ces bijoux, c'est honorer la culture, raconter une histoire et perpétuer un savoir-faire inestimable.</p>
+    <div class="qsm-img">
+      <img src="/public/img/sqm-bijoux.png" alt="Bijoux">
+    </div>
+  </div>
+</section>
+<section class="header">
+  <img src="/public/img/qsm-viana.png" alt="Photo de la cathédrale Santa Luzia de Viana Do Castelo" class="header-img viana">
+</section>
+<div class="qsm-last">
+  <h2>Viana do Castelo</h2>
+  <p>Viana do Castelo, perle du nord du Portugal, est un véritable joyau. En tant que fier natif, je célèbre chaque coin de cette ville, des majestueux jardins de la Praça da República à l'imposante basilique de Santa Luzia, qui surplombe la ville avec une vue à couper le souffle. Le fleuve Lima ajoute une sérénité unique, tandis que nos fêtes traditionnelles, comme la Romaria da Senhora d'Agonia, vibrent d'histoire et de fierté locale.</p>
+</div>
+<?php
+require_once(__DIR__ . "/../partials/footer.php");
+
+?>
--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -14,77 +14,82 @@
   <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
   <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
 </head>
-  <nav class="navbar" id="navbar">
-    <div class="navbarLinkContainer">
-      <div class="hamburgerIcon">
-        <i class="fa-solid fa-bars hamburgerIcon" id="hamburgerIcon"></i>
-        <i class="fa-solid fa-xmark closeIcon" id="closeIcon"></i>
-      </div>
-      <div class="navLinkleft">
-        <a href="/allArticle">Nouveauté</a>
-        <a href="">Catégories</a>
-        <a href="">Recherche</a>
-      </div>
-      <div class="logoContainer">
-        <a href="/"><img src="/public/img/Logo.png" alt="Logo de Trésor de Viana" class="logo"></a>
-      </div>
-      <div class="navLinkright">
-        <?php
-        if(isset($_SESSION['user'])){
-          ?>
-            <a href="/cart">Panier</a>
-            <a href="/profile?id=<?= $_SESSION['user']['idUser'] ?>">Compte</a>  
-          <?php
-        } else {
-          ?>
-            <a href="/register">Inscription</a>
-            <a href="/login">Connexion</a>
-          <?php
-        }
-?>
-       </ul>
-      </div>
+<nav class="navbar" id="navbar">
+  <div class="navbarLinkContainer">
+    <div class="hamburgerIcon">
+      <i class="fa-solid fa-bars hamburgerIcon" id="hamburgerIcon"></i>
+      <i class="fa-solid fa-xmark closeIcon" id="closeIcon"></i>
+    </div>
+    <div class="navLinkleft">
+      <a href="/allArticle">Nouveauté</a>
+      <a href="">Catégories</a>
+      <a href="">Recherche</a>
+    </div>
+    <div class="logoContainer">
+      <a href="/"><img src="/public/img/Logo.png" alt="Logo de Trésor de Viana" class="logo"></a>
+    </div>
+    <div class="navLinkright">
       <?php
-      if(isset($_SESSION['user'])){
-        ?>
-          <div class="smLinkContainer">
-          <a href="/cart" class="nav-link">Panier</a></div>
-        <?php
+      if (isset($_SESSION['user'])) {
+      ?>
+        <a href="/cart">Panier</a>
+        <a href="/profile?id=<?= $_SESSION['user']['idUser'] ?>">Compte</a>
+      <?php
       } else {
-        ?>
-          <div class="smLinkContainer">
-          <a href="/register" class="nav-link">Connexion</a></div>
-        <?php
+      ?>
+        <a href="/register">Inscription</a>
+        <a href="/login">Connexion</a>
+      <?php
       }
       ?>
-      
+      </ul>
     </div>
-    
-  </nav>
-  <div class="offsetcanva" id="offsetCanva">
-      <div class="offsetcanvaContainer">
-        <div>
-          <h5>Catégories</h5>
-          <ul>
-            <li><a href="/allArticle?category=bracelet">Bracelets</a></li>
-            <li><a href="/allArticle?category=collier">Colliers</a></li>
-            <li><a href="/allArticle?category=boucles">Boucles</a></li>
-            <li><a href="/allArticle?category=chale">Châles</a></li>
-          </ul>
-        </div>
-        <div>
-        <a href="/allArticle"><h5>Nouveauté</h5></a>
-        </div>
-        <div>
-          <?php
-            if(isset($_SESSION['user'])){
-              ?>
-                 <h5></a href="/profile">Compte<a></h5>
-              <?php
-            }
-          ?>
-         
-        </div>
+    <?php
+    if (isset($_SESSION['user'])) {
+    ?>
+      <div class="smLinkContainer">
+        <a href="/cart" class="nav-link">Panier</a>
       </div>
+    <?php
+    } else {
+    ?>
+      <div class="smLinkContainer">
+        <a href="/register" class="nav-link">Connexion</a>
+      </div>
+    <?php
+    }
+    ?>
+
+  </div>
+
+</nav>
+<div class="offsetcanva" id="offsetCanva">
+  <div class="offsetcanvaContainer">
+    <div>
+      <h5>Catégories</h5>
+      <ul>
+        <li><a href="/allArticle?category=bracelet">Bracelets</a></li>
+        <li><a href="/allArticle?category=collier">Colliers</a></li>
+        <li><a href="/allArticle?category=boucles">Boucles</a></li>
+        <li><a href="/allArticle?category=chale">Châles</a></li>
+      </ul>
     </div>
+    <div>
+      <a href="/allArticle">
+        <h5>Nouveauté</h5>
+      </a>
+    </div>
+    <div>
+      <?php
+      if (isset($_SESSION['user'])) {
+      ?>
+        <h5><a href="/profile">Compte</a></h5>
+      <?php
+      }
+      ?>
+
+    </div>
+  </div>
+</div>
+
 <body>
